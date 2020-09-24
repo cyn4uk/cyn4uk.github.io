2c0cb3e37465d6a99e2cdbe8e37d38fea384df39
OlgaGrach <Scyn4uk@gmail.com>
2020-09-24 17:41:13 +0300

    fon

---
+++ js/video.js
@@ -0,0 +1,45 @@
+vid = document.getElementById('v1');
+ 
+vid.addEventListener('mouseenter', function() {
+  vid.play();
+});
+
+vid.addEventListener('mouseleave', function() {
+  vid.pause();
+});
+
+
+
+vid2 = document.getElementById('v2');
+
+vid2.addEventListener('mouseenter', function() {
+  vid2.play();
+});
+
+vid2.addEventListener('mouseleave', function() {
+  vid2.pause();
+});
+
+
+
+vid3 = document.getElementById('v3');
+
+vid3.addEventListener('mouseenter', function() {
+  vid3.play();
+});
+
+vid3.addEventListener('mouseleave', function() {
+  vid3.pause();
+});
+
+
+
+vid4 = document.getElementById('v4');
+
+vid4.addEventListener('mouseenter', function() {
+  vid4.play();
+});
+
+vid4.addEventListener('mouseleave', function() {
+  vid4.pause();
+});
